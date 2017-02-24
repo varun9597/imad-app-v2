d32ebbec1530e236fab36c5cf848933b4ae16903
varun9597 <panditpautrav@gmail.com>
2017-02-24 21:30:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,23 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+
+button.onclick = function() {
+  
+    //creat a request
+    var request = new XMLHttpRequest();
+    
+    //capture
+    
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //action
+            if(request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementbyId('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    }
+    
+    request.open('GET','http://varun9597.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
