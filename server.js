33cbc80080d8200ff73548464c706587b16aad86
varun9597 <panditpautrav@gmail.com>
2017-02-24 15:35:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,55 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content = {
+title: 'Article One',
+heading: 'Article One',
+content: ` 
+<p>
+        Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content
+    </p>
+    
+    <p>
+         Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Contentv
+    </p>
+    
+    <p>
+         Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Contentvv
+    </p> `
+};
 
+function createTemplate (data) {
+    
+var title = data.title;
+var heading = data.heading;
+var content = data.content;
+var htmlTemplate = ` 
+    <html>
+    <head>
+    <title>
+    ${title}
+    </title>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        
+        <h3> ${heading}</h3>
+        <div>
+            Date
+        </div>
+        ${content}
+        </div>
+    </body>
+    </html> `;
+    return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -15,7 +63,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article-one', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ res.send(createTemplate(articleOne));
 });
 
 
